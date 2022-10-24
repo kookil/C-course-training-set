c589e1a34973ee137b051a4c4cd3771ed193abe5
kookil <kookil12020619@gmail.com>
2022-10-24 16:53:16 +0800

    这是注释内容

---
+++ Paint bucket/油漆桶/油漆桶/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace 油漆桶
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(145, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(63, 74);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(311, 331);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(486, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 32);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "开始画画";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(401, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(321, 330);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Paint bucket/油漆桶/油漆桶/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 油漆桶
+{
+    public partial class Form1 : Form
+    {
+
+        char SPLIT_SEPARATOR = ' ';
+        public Form1()
+        {
+            InitializeComponent();
+            this.SizeChanged += new Resize(this).Form1_Resize;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int line_max_count = 0;//计算这个二维表的列最多去到多少
+            List<List<String>> T = new List<List<String>>();//用于存二维表的容器，就是一个存List的List
+            OpenFileDialog openFileDialog1 = new System.Windows.Forms.OpenFileDialog();//一个打开文件的对话框    
+            openFileDialog1.Filter = "文本文件(*.csv)|*.csv"; 
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)//判断是否选择了文件      
+            {
+                StreamReader streamReader = new StreamReader(openFileDialog1.FileName, Encoding.Default);//读取文件的流
+                while (!streamReader.EndOfStream)//如果没读到最后
+                {
+                    List<String> line = new List<String>(streamReader.ReadLine().Split(SPLIT_SEPARATOR));//每一行根据分隔符形成数组，同时形成List
+                    T.Add(line);//T增加这一行
+                    if (line_max_count < line.Count)//此乃求列的最大值算法
+                    {
+                        line_max_count = line.Count;
+                    }
+                }
+                streamReader.Close();
+            }
+            //设置listview的表头  
+            for (int i = 0; i < line_max_count; i++)
+            {
+                listView1.Columns.Add("列" + (i + 1), listView1.Width / line_max_count - 1, HorizontalAlignment.Left);
+            }
+            listView1.BeginUpdate();//数据更新，UI暂时挂起，直到EndUpdate绘制控件，可以有效避免闪烁并大大提高加载速度  
+            for (int i = 0; i < T.Count; i++)
+            {
+                List<String> line = T[i];
+                ListViewItem listViewItem = new ListViewItem();
+                for (int j = 0; j < line.Count; j++)
+                {
+                    if (j == 0)
+                    {
+                        listViewItem.Text = line[j];//listview的每一行的第一项  
+                    }
+                    else
+                    {
+                        listViewItem.SubItems.Add(line[j]);//其余子项
+                    }
+                }
+                listView1.Items.Add(listViewItem);//将这行添加到listview中
+                //Console.WriteLine(listViewItem);
+            }
+            listView1.EndUpdate();//结束数据处理，UI界面一次性绘制。  
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        class Resize
+        {
+            private Form _form;
+
+            public Resize(Form form)
+            {
+                int count = form.Controls.Count * 2 + 2;
+                float[] factor = new float[count];
+                int i = 0;
+                factor[i++] = form.Size.Width;
+                factor[i++] = form.Size.Height;
+                foreach (Control ctrl in form.Controls)
+                {
+                    factor[i++] = ctrl.Location.X / (float)form.Size.Width;
+                    factor[i++] = ctrl.Location.Y / (float)form.Size.Height;
+                    ctrl.Tag = ctrl.Size;
+                }
+                form.Tag = factor;
+                this._form = form;
+            }
+
+            public void Form1_Resize(object sender, EventArgs e)
+            {
+                float[] scale = (float[])this._form.Tag;
+                int i = 2;
+                foreach (Control ctrl in this._form.Controls)
+                {
+                    ctrl.Left = (int)(this._form.Size.Width * scale[i++]);
+                    ctrl.Top = (int)(this._form.Size.Height * scale[i++]);
+                    ctrl.Width = (int)(this._form.Size.Width / (float)scale[0] * ((Size)ctrl.Tag).Width);
+                    ctrl.Height = (int)(this._form.Size.Height / (float)scale[1] * ((Size)ctrl.Tag).Height);
+                }
+            }
+        }
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(this.Height, this.Width); 
+            Graphics g = Graphics.FromImage(bitmap);
+            g.DrawString("GDI+ 技术", new Font("宋体", 12), new SolidBrush(Color.Red), this.Width / 3, 0);
+
+            g.DrawLine(new Pen(Color.Yellow, 12), 0, 40, this.Width, 40);
+
+            g.DrawLine(new Pen(Color.Red, 0.1F), 0, 80, this.Width, 80);
+            this.BackgroundImage = bitmap;
+            g.Dispose();//*/
+
+            //数据初始化   
+            int[] X = new int[5] { 1, 2, 3, 4, 5 };
+            int[] Y = new int[5] {1, 2, 3, 4, 5};
+            //画图初始化   
+            Bitmap bMap = new Bitmap(500, 500);
+            Graphics gph = Graphics.FromImage(bMap);
+            gph.Clear(Color.White);
+
+            PointF cPt = new PointF(40, 420);//中心点(坐标轴原点)   
+            PointF[] xPt = new PointF[3]{
+             new   PointF(cPt.Y+5,cPt.Y),
+             new   PointF(cPt.Y,cPt.Y-5),
+             new   PointF(cPt.Y,cPt.Y+5)};//(X轴右边三角形的三个点坐标）   
+            PointF[] yPt = new PointF[3]{
+             new   PointF(cPt.X,cPt.X-5),
+             new   PointF(cPt.X+5,cPt.X),
+             new   PointF(cPt.X-5,cPt.X)};//Y轴三角形   
+            gph.DrawString("作图", new Font("宋体", 14),
+             Brushes.Black, new PointF(cPt.X + 12, cPt.X));//图表标题   
+            //画X轴   
+            gph.DrawLine(Pens.Black, cPt.X, cPt.Y, cPt.Y, cPt.Y);//画x轴
+            gph.DrawPolygon(Pens.Black, xPt);//画X轴箭头三角形
+            gph.FillPolygon(new SolidBrush(Color.Black), xPt);//X轴箭头填充黑色
+            gph.DrawString("x轴", new Font("宋体", 12),
+             Brushes.Black, new PointF(cPt.Y + 12, cPt.Y - 10));
+            //画Y轴   
+            gph.DrawLine(Pens.Black, cPt.X, cPt.Y, cPt.X, cPt.X);
+            gph.DrawPolygon(Pens.Black, yPt);
+            gph.FillPolygon(new SolidBrush(Color.Black), yPt);
+            gph.DrawString("y轴", new Font("宋体", 12), Brushes.Black, new PointF(6, 7));
+
+
+            }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
+
